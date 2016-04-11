60a0a8fae78ee5e74360cb22f253becb00d97e5f
drubino <doug.rubino@gmail.com>
2016-04-10 22:41:41 -0400

    Created Token and ParseTreeNode

---
+++ DragonBook.RecursiveDescentParsing/ParseTreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBook.RecursiveDescentParsing
+{
+    public class ParseTreeNode
+    {
+        public string Type { get; private set; }
+        public Token Token { get; private set; }
+        public bool IsTerminal { get { return this.Token != null; } }
+        public IEnumerable<ParseTreeNode> Children { get; private set; }
+        
+        public ParseTreeNode(string type, Token token)
+        {
+            this.Type = type;
+            this.Token = token;
+        }
+
+        public ParseTreeNode(string type, IEnumerable<ParseTreeNode> children)
+        {
+            this.Type = type;
+            this.Children = children;
+        }
+        
+        public override string ToString()
+        {
+            return this.IsTerminal ? 
+                this.Token.Value : 
+                string.Join("", this.Children);
+        }
+    }
+}
---
+++ DragonBook.RecursiveDescentParsing/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonBook.RecursiveDescentParsing
+{
+    public class Token
+    {
+        public string Type { get; private set; }
+        public string Value { get; private set; }
+
+        public Token(string type, string value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+    }
+}
